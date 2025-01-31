74b08eecdab21b3ec687b7c1b291d336fe4dd875
aashnajoshi <aashnahetal@gmail.com>
2025-01-31 13:57:19 +0530

    .

--- home/views.py
+++ home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home/index.html', context={'title': 'Home'})
+
+def about(request):
+    return render(request, 'home/index.html', context={'title': 'About'})
