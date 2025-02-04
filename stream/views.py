617ed3f9493ff95f0afec50be97d5e65b03f4d46
aashnajoshi <aashnahetal@gmail.com>
2025-02-04 09:29:22 +0530

    .

--- stream/views.py
+++ stream/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Stream
+from home.views import signin
+from django.http import HttpResponseRedirect
 
 # Display list of streams on home page
 def home(request):
@@ -12,6 +15,15 @@ def stream(request, slug):
     return render(request, 'stream/stream.html', context={'title': stream.title, 'stream': stream})
 
 # Display the video play page
+@login_required
 def video_play(request, id):
     stream = get_object_or_404(Stream, id=id)  # Fetch stream by id
     return render(request, 'stream/stream.html', context={'title': stream.title, 'stream': stream, 'play_video': True})
+
+# Display the sign-in page
+def signin_check(request):
+    if request.method == 'POST':
+        signin(request)
+        next_url = request.GET.get('next', '/')
+        return HttpResponseRedirect(next_url)
+    return render(request, 'stream/stream.html')
