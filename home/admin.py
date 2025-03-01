f476d41ef6d5520722ec06df054b91b536879ca0
aashnajoshi <aashnahetal@gmail.com>
2025-03-01 22:53:25 +0530

    .

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,12 @@
 from django.contrib.admin import register
 from unfold.admin import ModelAdmin
+from unfold.contrib.forms.widgets import WysiwygWidget
+from django.db import models
 from .models import Contact
 
 @register(Contact)
 class ContactAdmin(ModelAdmin):
+    formfield_overrides = {models.TextField: {"widget": WysiwygWidget}}
     list_display = ('name', 'email', 'message', 'file_path', 'created_at')
     search_fields = ('name', 'email', 'message')
     list_filter = ('name','created_at')
--- stream/admin.py
+++ stream/admin.py
@@ -1,21 +1,26 @@
 from django.contrib.admin import register
 from unfold.admin import ModelAdmin
+from unfold.contrib.forms.widgets import WysiwygWidget
+from django.db import models
 from .models import *
 
 @register(Stream)
 class StreamAdmin(ModelAdmin):
+    formfield_overrides = {models.TextField: {"widget": WysiwygWidget}}
     list_display = ('title', 'genre', 'created_at', 'uploaded_by', 'views')
     search_fields = ('title', 'genre', 'description')
     list_filter = ('genre', 'created_at')
 
 @register(Comment)
 class CommentAdmin(ModelAdmin):
+    formfield_overrides = {models.TextField: {"widget": WysiwygWidget}}
     list_display = ('video', 'user', 'content', 'created_at')
     search_fields = ('video', 'user', 'created_at')
     list_filter = ('video', 'user', 'created_at')
 
 @register(Room)
 class RoomAdmin(ModelAdmin):
+    formfield_overrides = {models.TextField: {"widget": WysiwygWidget}}
     list_display = ('room_code', 'created_at')
     search_fields = ('room_code', 'created_at')
     list_filter = ('room_code', 'created_at')
