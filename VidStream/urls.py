fdc0348521ec78deec1e3de5152b870622fc03a8
aashnajoshi <aashnahetal@gmail.com>
2025-04-15 21:53:45 +0530

    .

--- VidStream/urls.py
+++ VidStream/urls.py
@@ -6,11 +6,11 @@ from django.urls import include, path
 from home.views import SearchView, SignUpView, SignInView, SignOutView
 
 urlpatterns = [
-    path('', include('home.urls')),
+    path('', include('home.urls'), name='home'),
     path('admin/', admin.site.urls),
-    path('debug/', include(debug_toolbar.urls)),
-    path('stream/', include('stream.urls')),
     path('accounts/', include('allauth.urls')),
+    path('debug/', include(debug_toolbar.urls)),
+    path('stream/', include('stream.urls'), name='stream'),
     path('search/', SearchView.as_view(), name='search'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('signin/', SignInView.as_view(), name='signin'),
