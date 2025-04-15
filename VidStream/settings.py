ab69dac40ca59c01f3ddfd6ac7a18a01d32ed6cd
aashnajoshi <aashnahetal@gmail.com>
2025-04-15 18:25:06 +0530

    .

--- VidStream/settings.py
+++ VidStream/settings.py
@@ -103,8 +103,6 @@ DATABASES = {
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
     }
 }
 
