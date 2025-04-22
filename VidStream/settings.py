1a28077b450054a5af58bc29c2adfbe19de37cb8
kkadk <125792968+kkadk@users.noreply.github.com>
2025-04-22 11:25:15 +0545

    redis implementation instead of InMemory

--- VidStream/settings.py
+++ VidStream/settings.py
@@ -184,6 +184,9 @@ ASGI_APPLICATION = 'VidStream.asgi.application'
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
     },
 }
